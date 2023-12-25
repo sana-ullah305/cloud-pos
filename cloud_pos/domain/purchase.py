c2751ca6a3911f8421fcf71bebb1a36b3f666d50
DESKTOP-MPCDS0I\sanaullah <sana.ullah305@outlook.com>
2023-12-26 02:36:25 +0500

    updated: /domain/purchase implemented purchase entity

--- cloud_pos/domain/purchase.py
+++ cloud_pos/domain/purchase.py
@@ -1,5 +1,11 @@
 # cloud_pos/domain/purchase.py
 class Purchase:
-    # Implement Purchase entity
-    def __init__(self) -> None:
-        pass
+    def __init__(self, purchase_id, products):
+        self.purchase_id = purchase_id
+        self.products = products
+        self.total_cost = sum(product.price for product in products)
+
+# Example usage:
+# product1 = Product(product_id=1, name="Product1", price=10.0, stock_quantity=50)
+# product2 = Product(product_id=2, name="Product2", price=20.0, stock_quantity=30)
+# purchase = Purchase(purchase_id=1, products=[product1, product2])
