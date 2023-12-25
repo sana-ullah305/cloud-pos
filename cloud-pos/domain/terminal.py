767eb4a2b131bf66c85a26ab8b42163e9a2b057c
DESKTOP-MPCDS0I\sanaullah <sana.ullah305@outlook.com>
2023-12-26 01:28:30 +0500

    added: /domain/terminal.py file and basic entity defined

---
+++ cloud-pos/domain/terminal.py
@@ -0,0 +1,9 @@
+class Terminal:
+    def __init__(self, terminal_id, location):
+        self.terminal_id = terminal_id
+        self.location = location
+        self.transactions = []
+
+    def process_transaction(self, transaction):
+        # Business logic to process a transaction
+        pass
