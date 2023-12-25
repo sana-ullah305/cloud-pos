8867dd09747e517a87e6dd558502fc4143e6023f
DESKTOP-MPCDS0I\sanaullah <sana.ullah305@outlook.com>
2023-12-26 01:34:03 +0500

    added: cloud_pos/domain/store.py file and basic function

---
+++ cloud-pos/repositories/store.py
@@ -0,0 +1,9 @@
+class Store:
+    def __init__(self, store_id, name):
+        self.store_id = store_id
+        self.name = name
+        self.terminals = []
+
+    def add_terminal(self, terminal):
+        # Business logic to add a terminal to the store
+        pass
