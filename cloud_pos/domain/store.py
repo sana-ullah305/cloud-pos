c9f0ea6d45030515a3fedb36ce446d9a7de952ec
DESKTOP-MPCDS0I\sanaullah <sana.ullah305@outlook.com>
2023-12-26 02:26:48 +0500

    updated: domain/store method name changed transaction -> add_product

--- cloud_pos/domain/store.py
+++ cloud_pos/domain/store.py
@@ -1,9 +1,11 @@
+# cloud_pos/domain/store.py
 class Store:
     def __init__(self, store_id, name):
         self.store_id = store_id
         self.name = name
         self.terminals = []
+        self.products = []
 
-    def add_terminal(self, terminal):
-        # Business logic to add a terminal to the store
+    def add_product(self, product):
+        # Business logic to add a product to the store
         pass
