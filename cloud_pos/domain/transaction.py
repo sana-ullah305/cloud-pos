19acec1f4a9cc6f4786721abaeb98bebf7f09ae1
DESKTOP-MPCDS0I\sanaullah <sana.ullah305@outlook.com>
2023-12-26 02:23:05 +0500

    updated: /domain/transaction.py  Modify Transaction Entity to Handle Sales and Purchases.

--- cloud_pos/domain/transaction.py
+++ cloud_pos/domain/transaction.py
@@ -1,12 +1,15 @@
+# cloud_pos/domain/transaction.py
 class Transaction:
     def __init__(self, transaction_id):
         self.transaction_id = transaction_id
         self.products = []
+        self.sale = None
+        self.purchase = None
 
-    def add_product(self, product, quantity):
-        # Business logic to add a product to the transaction
+    def add_sale(self, sale):
+        # Business logic to associate a sale with the transaction
         pass
 
-    def complete_transaction(self):
-        # Business logic to complete the transaction
+    def add_purchase(self, purchase):
+        # Business logic to associate a purchase with the transaction
         pass
