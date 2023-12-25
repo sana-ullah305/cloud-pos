aa9ff5b18fcbf7aa80b38fa2bdf5bb41a50bc8d7
DESKTOP-MPCDS0I\sanaullah <sana.ullah305@outlook.com>
2023-12-26 02:35:35 +0500

    updated: /domain/sale implemented sales

--- cloud_pos/domain/sale.py
+++ cloud_pos/domain/sale.py
@@ -1,5 +1,11 @@
 # cloud_pos/domain/sale.py
 class Sale:
-    # Implement Sale entity
-    def __init__(self) -> None:
-        pass
+    def __init__(self, sale_id, products):
+        self.sale_id = sale_id
+        self.products = products
+        self.total_amount = sum(product.price for product in products)
+
+# Example usage:
+# product1 = Product(product_id=1, name="Product1", price=10.0, stock_quantity=50)
+# product2 = Product(product_id=2, name="Product2", price=20.0, stock_quantity=30)
+# sale = Sale(sale_id=1, products=[product1, product2])
