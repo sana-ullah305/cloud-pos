49e7e6fc825ef8ee73dddcbc44b9d82300d4b09a
DESKTOP-MPCDS0I\sanaullah <sana.ullah305@outlook.com>
2023-12-26 11:53:27 +0500

    updated: refactor /domain/porduct.py & /repositories/product_repository.py some other methods are added

--- cloud_pos/domain/product.py
+++ cloud_pos/domain/product.py
@@ -1,20 +1,41 @@
 # cloud_pos/domain/product.py
 class Product:
     def __init__(self, product_id, name, price, stock_quantity):
-        self.product_id = product_id
-        self.name = name
-        self.price = price
-        self.stock_quantity = stock_quantity
+        self._product_id = product_id
+        self._name = name
+        self._price = price
+        self._stock_quantity = stock_quantity
 
-    def update_stock(self, quantity):
-        # Check if there is enough stock to update
-        if self.stock_quantity + quantity >= 0:
-            self.stock_quantity += quantity
-            return True  # Stock updated successfully
-        else:
-            return False  # Insufficient stock
+    @property
+    def product_id(self):
+        return self._product_id
 
-# Example usage:
-# product = Product(product_id=1, name="Product1", price=10.0, stock_quantity=50)
-# product.update_stock(-10)  # Decrease stock by 10
-# product.update_stock(20)   # Increase stock by 20
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def stock_quantity(self):
+        return self._stock_quantity
+
+    def increase_stock(self, quantity):
+        """Increase the stock quantity."""
+        self._stock_quantity += quantity
+
+    def decrease_stock(self, quantity):
+        """Decrease the stock quantity if there is sufficient stock."""
+        if self.is_stock_sufficient:
+            self._stock_quantity -= quantity
+
+    @property
+    def is_stock_sufficient(self):
+        """Check if the stock is sufficient for a given operation."""
+        return self._stock_quantity >= 0
+
+    def __repr__(self):
+        """String representation of the Product."""
+        return f"Product(ID: {self._product_id}, Name: {self._name}, Price: {self._price}, Stock: {self._stock_quantity})"
--- cloud_pos/repositories/product_repository.py
+++ cloud_pos/repositories/product_repository.py
@@ -1,14 +1,57 @@
 # cloud_pos/repositories/product_repository.py
+from cloud_pos.domain.product import Product
+
 class ProductRepository:
-    products = []
+    def __init__(self):
+        """Initialize the ProductRepository."""
+        # Using a dictionary to simulate an in-memory data storage
+        self.products = {}
 
-    @classmethod
-    def save_product(cls, product):
-        cls.products.append(product)
+    def add_product(self, product):
+        """Add a product to the repository.
 
-    @classmethod
-    def get_product_by_id(cls, product_id):
-        for product in cls.products:
-            if product.product_id == product_id:
-                return product
-        return None
+        Args:
+            product (Product): The product object to be added.
+        """
+        self.products[product.product_id] = product
+
+    def update_product(self, product_id, new_product_data):
+        """Update product information in the repository.
+
+        Args:
+            product_id (int): The ID of the product to be updated.
+            new_product_data (dict): A dictionary containing new data for the product.
+        """
+        product = self.products.get(product_id)
+        if product:
+            product._name = new_product_data.get('name', product._name)
+            product._price = new_product_data.get('price', product._price)
+            product.increase_stock(new_product_data.get('stock_quantity', 0))
+
+    def get_product_by_id(self, product_id):
+        """Retrieve a product by its ID.
+
+        Args:
+            product_id (int): The ID of the product to retrieve.
+
+        Returns:
+            Product: The product object if found, otherwise None.
+        """
+        return self.products.get(product_id)
+
+    def get_all_products(self):
+        """Retrieve all products in the repository.
+
+        Returns:
+            list: A list containing all product objects.
+        """
+        return list(self.products.values())
+
+    def delete_product(self, product_id):
+        """Delete a product from the repository.
+
+        Args:
+            product_id (int): The ID of the product to be deleted.
+        """
+        if product_id in self.products:
+            del self.products[product_id]
